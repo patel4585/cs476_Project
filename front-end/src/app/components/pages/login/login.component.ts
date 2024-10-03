2dd8ec042ca7b65d6266eddbc2e292aa6cb01275
Dev <devpatel4585@gmail.com>
2024-10-03 15:19:02 -0600

    login page front-end basic code optimization

--- front-end/src/app/components/pages/login/login.component.ts
+++ front-end/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
 
+  private linkElement: HTMLLinkElement | null = null;
+
+  ngOnInit(): void {
+    this.linkElement = document.createElement('link');
+
+    this.linkElement.rel = 'stylesheet';
+    this.linkElement.href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css';
+    document.head.appendChild(this.linkElement);
+
+    document.body.classList.add('login-page-body');
+  }
+
+  ngOnDestroy(): void {
+    document.body.classList.remove('login-page-body');
+    if (this.linkElement){
+      document.head.removeChild(this.linkElement);
+      this.linkElement = null;
+    }
+  }
 }
