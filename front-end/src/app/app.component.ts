7478777bf2c3bfd40e871a6a8a44500a6c7aaa66
CathyCat87 <124227596+CathyCat87@users.noreply.github.com>
2024-10-15 23:26:53 -0600

    Home Page Initiate

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {LoginComponent} from "./components/pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
