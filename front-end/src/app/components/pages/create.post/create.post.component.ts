6a5d48ec010709bcfa9b994a999439547681df07
CathyCat87 <124227596+CathyCat87@users.noreply.github.com>
2024-10-21 22:30:14 -0600

    Create a post component

--- front-end/src/app/components/pages/create.post/create.post.component.ts
+++ front-end/src/app/components/pages/create.post/create.post.component.ts
@@ -3,8 +3,10 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm, Validators,ReactiveFormsModule} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { DatePipe } from '@angular/common';
+
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -13,14 +15,21 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
+
+
+
 @Component({
   selector: 'create.post',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatSelectModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatSelectModule,ReactiveFormsModule],
   templateUrl: './create.post.component.html',
   styleUrl: './create.post.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DatePipe]
 })
+
+
+
 export class CreatePostComponent{
   protected readonly value = signal('');
 
@@ -34,5 +43,8 @@ export class CreatePostComponent{
   ]);
 
   matcher = new MyErrorStateMatcher();
+
+  today = new Date();
+
 
 }
