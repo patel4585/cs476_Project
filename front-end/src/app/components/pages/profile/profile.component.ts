c3df02a1af7391fb02c6d4dd89915feeb4b0ca5a
Ahsoka.13 <patelvrushti1311@gmail.com>
2024-11-04 09:46:21 -0600

    Profile page commit

--- front-end/src/app/components/pages/profile/profile.component.ts
+++ front-end/src/app/components/pages/profile/profile.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  standalone: true,
-  imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  constructor() {}
 
+  onUpdateProfile(firstName: string, lastName: string, email: string, newPassword: string, confirmNewPassword: string) {
+    if (newPassword && newPassword !== confirmNewPassword) {
+      alert('New password and confirmation do not match.');
+      return;
+    }
+    // need to add backend here to update data -- @Devpatel
+    console.log('Updated profile details:', { firstName, lastName, email });
+    alert('Profile updated successfully!');
+  }
 }
