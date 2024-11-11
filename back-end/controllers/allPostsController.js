5eecfb496dfe1d404162c88d1b9ba60b74d3d05e
Dev <devpatel4585@gmail.com>
2024-11-11 14:55:01 -0600

    Backend-test-allPostsController

--- back-end/controllers/allPostsController.js
+++ back-end/controllers/allPostsController.js
@@ -5,5 +5,5 @@ const User = require("../models/user");
 exports.allPosts_List = asyncHandler(async (req, res, next) => {
     const allPosts = await Post.find().sort({ updatedAt: -1 }).populate('user').exec();
     
-    res.json(allPosts);
+    res.status(200).json(allPosts);
 });
