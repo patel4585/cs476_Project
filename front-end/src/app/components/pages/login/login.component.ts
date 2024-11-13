0d2896a43058678f89cd8646aed68b849b9dff68
Ashoka.13 <patelvrushti1311@gmail.com>
2024-11-13 01:41:36 -0600

    login-page-validation

--- front-end/src/app/components/pages/login/login.component.ts
+++ front-end/src/app/components/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ FormsModule, RouterLink ],
+  imports: [ FormsModule, RouterLink, NgIf ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,6 +16,8 @@ export class LoginComponent implements OnInit, OnDestroy {
   private linkElement: HTMLLinkElement | null = null;
   email: string = "";
   password: string = "";
+  error_message_hidden: boolean = true;
+  error_message: string = "";
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -40,17 +43,27 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   onLogin(): void {
+    
+    if (!this.email || !this.password) {
+      this.error_message_hidden = false;
+      this.error_message = "Please fill out all required fields.";
+      return;
+    }
+
     this.authService.login(this.email, this.password).subscribe({
       next: (response) => {
         if(response.success) {
           console.log("Login Successful", response.user);
           this.router.navigate(['/home']);
         } else {
-          console.log("Login Failed");
+          this.error_message_hidden = false;
+          this.error_message = "Invalid email or password";
+
         }
       },
       error: (error) => {
-        console.log('An error occurred', error.message);
+        this.error_message_hidden = false;
+        this.error_message = "An error occurred during logging in - " + error.message;
       }
     })
   }
