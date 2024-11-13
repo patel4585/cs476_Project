c914559c2141aa7089f707934abd227fa72feef8
Vrushti Patel <patelvrushti1311@gmail.com>
2024-11-12 23:34:49 -0600

    signup-link-added

--- front-end/src/app/components/pages/login/login.component.ts
+++ front-end/src/app/components/pages/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ FormsModule ],
+  imports: [ FormsModule, RouterLink ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
